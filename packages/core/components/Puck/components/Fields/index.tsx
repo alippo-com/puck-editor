27d890c0677d3734ec8f7f1f4a9838b1f520100e
princebansal <prince@alippo.com>
2024-09-03 17:58:38 +0530

    Change field editor ui to bottom sheet and spread fields horizontally

--- packages/core/components/Puck/components/Fields/index.tsx
+++ packages/core/components/Puck/components/Fields/index.tsx
@@ -1,4 +1,10 @@
-import { Loader } from "../../../Loader";
+import { Button, Drawer, Space } from "antd";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState
+} from "react";
 import { rootDroppableId } from "../../../../lib/root-droppable-id";
 import {
   ReplaceAction,
@@ -8,22 +14,18 @@ import {
 } from "../../../../reducer";
 import {
   ComponentData,
-  DefaultComponentProps,
   RootData,
-  UiState,
-  Permissions,
-  Config,
+  UiState
 } from "../../../../types/Config";
 import type { Field, Fields as FieldsType } from "../../../../types/Fields";
 import { AutoFieldPrivate } from "../../../AutoField";
+import { Loader } from "../../../Loader";
 import { useAppContext } from "../../context";
 
-import styles from "./styles.module.css";
 import { getClassNameFactory } from "../../../../lib";
-import { ReactNode, useCallback, useEffect, useMemo, useState } from "react";
-import { ItemSelector } from "../../../../lib/get-item";
 import { getChanged } from "../../../../lib/get-changed";
 import { getPermissions } from "../../../../lib/get-permissions";
+import styles from "./styles.module.css";
 
 const getClassName = getClassNameFactory("PuckFields", styles);
 
@@ -34,9 +36,9 @@ const defaultPageFields: Record<string, Field> = {
 const DefaultFields = ({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
   isLoading: boolean;
-  itemSelector?: ItemSelector | null;
+  itemSelector?: any;
 }) => {
   return <>{children}</>;
 };
@@ -164,6 +166,17 @@ export const Fields = () => {
   const rootProps = data.root.props || data.root;
 
   const Wrapper = useMemo(() => overrides.fields || DefaultFields, [overrides]);
+  const [open, setOpen] = useState(false);
+  const [currentField, setCurrentField] = useState<string | null>(null);
+
+  const showDrawer = (fieldName: string) => {
+    setCurrentField(fieldName);
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   return (
     <form
@@ -173,123 +186,163 @@ export const Fields = () => {
       }}
     >
       <Wrapper isLoading={isLoading} itemSelector={itemSelector}>
-        {Object.keys(fields).map((fieldName) => {
-          const field = fields[fieldName];
+        <div
+          className="button-row"
+          style={{
+            display: "flex",
+            gap: "8px",
+            overflowX: "auto",
+            padding: "8px",
+          }}
+        >
+          {Object.keys(fields).map((fieldName) => (
+            <Space key={fieldName}>
+              <Button
+                size="large"
+                onClick={() => showDrawer(fieldName)}
+                className="gradient-button"
+              >
+                {fieldName}
+              </Button>
+            </Space>
+          ))}
+        </div>
 
-          if (!field?.type) return null;
+        <Drawer
+          title={currentField}
+          placement="bottom"
+          closable
+          onClose={onClose}
+          open={open}
+          key="bottom"
+          height={"auto"}
+          mask={false}
+        >
+          {currentField && (
+            <div className="modal-content">
+              {Object.keys(fields).map((fieldName) => {
+                const field = fields[fieldName];
 
-          const onChange = (value: any, updatedUi?: Partial<UiState>) => {
-            let currentProps;
+                if (fieldName !== currentField || !field?.type) return null;
+                const onChange = (value: any, updatedUi?: Partial<UiState>) => {
+                  let currentProps;
 
-            if (selectedItem) {
-              currentProps = selectedItem.props;
-            } else {
-              currentProps = rootProps;
-            }
+                  if (selectedItem) {
+                    currentProps = selectedItem.props;
+                  } else {
+                    currentProps = rootProps;
+                  }
 
-            const newProps = {
-              ...currentProps,
-              [fieldName]: value,
-            };
+                  const newProps = {
+                    ...currentProps,
+                    [fieldName]: value,
+                  };
 
-            if (itemSelector) {
-              const replaceActionData: ReplaceAction = {
-                type: "replace",
-                destinationIndex: itemSelector.index,
-                destinationZone: itemSelector.zone || rootDroppableId,
-                data: { ...selectedItem, props: newProps },
-              };
+                  if (itemSelector) {
+                    const replaceActionData: ReplaceAction = {
+                      type: "replace",
+                      destinationIndex: itemSelector.index,
+                      destinationZone: itemSelector.zone || rootDroppableId,
+                      data: { ...selectedItem, props: newProps },
+                    };
 
-              // We use `replace` action, then feed into `set` action so we can also process any UI changes
-              const replacedData = replaceAction(data, replaceActionData);
+                    // We use `replace` action, then feed into `set` action so we can also process any UI changes
+                    const replacedData = replaceAction(data, replaceActionData);
 
-              const setActionData: SetAction = {
-                type: "set",
-                state: {
-                  data: { ...data, ...replacedData },
-                  ui: { ...ui, ...updatedUi },
-                },
-              };
-
-              // If the component has a resolveData method, we let resolveData run and handle the dispatch once it's done
-              if (config.components[selectedItem!.type]?.resolveData) {
-                resolveData(setAction(state, setActionData));
-              } else {
-                dispatch({
-                  ...setActionData,
-                  recordHistory: true,
-                });
-              }
-            } else {
-              if (data.root.props) {
-                // If the component has a resolveData method, we let resolveData run and handle the dispatch once it's done
-                if (config.root?.resolveData) {
-                  resolveData({
-                    ui: { ...ui, ...updatedUi },
-                    data: {
-                      ...data,
-                      root: { props: newProps },
-                    },
-                  });
-                } else {
-                  dispatch({
-                    type: "set",
-                    state: {
-                      ui: { ...ui, ...updatedUi },
-                      data: {
-                        ...data,
-                        root: { props: newProps },
+                    const setActionData: SetAction = {
+                      type: "set",
+                      state: {
+                        data: { ...data, ...replacedData },
+                        ui: { ...ui, ...updatedUi },
                       },
-                    },
-                    recordHistory: true,
+                    };
+
+                    // If the component has a resolveData method, we let resolveData run and handle the dispatch once it's done
+                    if (config.components[selectedItem!.type]?.resolveData) {
+                      resolveData(setAction(state, setActionData));
+                    } else {
+                      dispatch({
+                        ...setActionData,
+                        recordHistory: true,
+                      });
+                    }
+                  } else {
+                    if (data.root.props) {
+                      // If the component has a resolveData method, we let resolveData run and handle the dispatch once it's done
+                      if (config.root?.resolveData) {
+                        resolveData({
+                          ui: { ...ui, ...updatedUi },
+                          data: {
+                            ...data,
+                            root: { props: newProps },
+                          },
+                        });
+                      } else {
+                        dispatch({
+                          type: "set",
+                          state: {
+                            ui: { ...ui, ...updatedUi },
+                            data: {
+                              ...data,
+                              root: { props: newProps },
+                            },
+                          },
+                          recordHistory: true,
+                        });
+                      }
+                    } else {
+                      // DEPRECATED
+                      dispatch({
+                        type: "setData",
+                        data: { root: newProps },
+                      });
+                    }
+                  }
+                };
+
+                if (selectedItem && itemSelector) {
+                  const { readOnly = {} } = selectedItem;
+                  const { edit } = getPermissions({
+                    selectedItem,
+                    config,
+                    globalPermissions: globalPermissions || {},
+                    appState: state,
                   });
+
+                  return (
+                    <div key={`${selectedItem.props.id}_${fieldName}`}>
+                      <AutoFieldPrivate
+                        key={`${selectedItem.props.id}_${fieldName}`}
+                        field={field}
+                        name={fieldName}
+                        id={`${selectedItem.props.id}_${fieldName}`}
+                        readOnly={!edit || readOnly[fieldName]}
+                        value={selectedItem.props[fieldName]}
+                        onChange={onChange}
+                      />
+                    </div>
+                  );
+                } else {
+                  const { readOnly = {} } = data.root;
+
+                  return (
+                    <div key={`page_${fieldName}`}>
+                      <AutoFieldPrivate
+                        key={`page_${fieldName}`}
+                        field={field}
+                        name={fieldName}
+                        id={`root_${fieldName}`}
+                        readOnly={readOnly[fieldName]}
+                        value={rootProps[fieldName]}
+                        onChange={onChange}
+                      />
+                    </div>
+                  );
                 }
-              } else {
-                // DEPRECATED
-                dispatch({
-                  type: "setData",
-                  data: { root: newProps },
-                });
-              }
-            }
-          };
-
-          if (selectedItem && itemSelector) {
-            const { readOnly = {} } = selectedItem;
-            const { edit } = getPermissions({
-              selectedItem,
-              config,
-              globalPermissions: globalPermissions || {},
-              appState: state,
-            });
-
-            return (
-              <AutoFieldPrivate
-                key={`${selectedItem.props.id}_${fieldName}`}
-                field={field}
-                name={fieldName}
-                id={`${selectedItem.props.id}_${fieldName}`}
-                readOnly={!edit || readOnly[fieldName]}
-                value={selectedItem.props[fieldName]}
-                onChange={onChange}
-              />
-            );
-          } else {
-            const { readOnly = {} } = data.root;
-
-            return (
-              <AutoFieldPrivate
-                key={`page_${fieldName}`}
-                field={field}
-                name={fieldName}
-                id={`root_${fieldName}`}
-                readOnly={readOnly[fieldName]}
-                value={rootProps[fieldName]}
-                onChange={onChange}
-              />
-            );
-          }
-        })}
+              })}
+            </div>
+          )}
+        </Drawer>
       </Wrapper>
       {isLoading && (
         <div className={getClassName("loadingOverlay")}>
